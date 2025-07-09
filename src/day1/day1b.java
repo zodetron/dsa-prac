730dd82ed8794c5367124fbdac1da2ebc24ea624
hardik <hardikzode05@gmail.com>
2025-07-09 15:02:39 +0530

    Added Day X practice

---
+++ src/day1/day1b.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class day1b {
+    public static void main(String[] args) {
+        for(int i=0;i<=6;i++){
+            System.out.println("helloww");
+        }
+    }
+}
---
+++ src/day2/day2a.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class day2a {
+    public static void main(String[] args) {
+        for(int i=0;i<=6;i++){
+            System.out.println("helloww");
+        }
+    }
+}
