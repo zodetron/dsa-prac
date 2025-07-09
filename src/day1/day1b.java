4d04c36db2907020d1cbeef0b3b17671641fe1c4
hardik <hardikzode05@gmail.com>
2025-07-09 22:10:34 +0530

    .

--- src/day1/day1b.java
+++ src/day1/day1b.java
@@ -2,8 +2,14 @@ package day1;
 
 public class day1b {
     public static void main(String[] args) {
-        for(int i=0;i<=6;i++){
-            System.out.println("helloww");
+        Scanner sc=new Scanner(System.in);
+        int n = sc.nextInt();
+        int a=n;
+        for(int i=1;i<11;i++){
+//            a=n*i;
+            System.out.println(n*i);
+//            System.out.println("\n");
+
         }
     }
 }
