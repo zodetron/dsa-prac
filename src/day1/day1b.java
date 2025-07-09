4ca2cfaa90f94db0bfa2b92c85063e9d6772da52
hardik <hardikzode05@gmail.com>
2025-07-09 23:45:01 +0530

    Added day1 codes

--- src/day1/day1b.java
+++ src/day1/day1b.java
@@ -1,14 +1,13 @@
 package day1;
+import java.util.*;
 
 public class day1b {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n = sc.nextInt();
-        int a=n;
         for(int i=1;i<11;i++){
-//            a=n*i;
             System.out.println(n*i);
-//            System.out.println("\n");
+
 
         }
     }
--- src/day2/day2a.java
+++ src/day2/day2a.java
@@ -2,8 +2,7 @@ package day1;
 
 public class day2a {
     public static void main(String[] args) {
-        for(int i=0;i<=6;i++){
-            System.out.println("helloww");
+
         }
     }
-}
+
