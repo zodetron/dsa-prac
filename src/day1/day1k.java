5a144a89887e444477c2fe0d66be694616e60917
hardik <hardikzode05@gmail.com>
2025-07-11 12:13:21 +0530

    Added 11th july practice

---
+++ src/day1/day1k.java
@@ -0,0 +1,18 @@
+package day1;
+
+public class day1k {
+    public static void main(String[] args) {
+        int n=5;
+        int number=1;
+
+        for(int i=0;i<=n;i++){
+
+            for (int j=1;j<=i;j++){
+                System.out.print(number+" ");
+                number++;
+            }
+
+            System.out.println();
+        }
+    }
+}
