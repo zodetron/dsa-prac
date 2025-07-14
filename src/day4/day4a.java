4374a1afcd089fd86fd595b95d0a3ee491883565
hardik <hardikzode05@gmail.com>
2025-07-14 11:43:19 +0530

    Added 14th july practice

---
+++ src/day4/day4a.java
@@ -0,0 +1,14 @@
+package day4;
+import java.util.*;
+
+public class day4a {
+    public static void main(String[] args) {
+        int[] marks = new int[3];
+        marks[0]=97;
+        marks[1]=87;
+        marks[2]=72;
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+    }
+}
