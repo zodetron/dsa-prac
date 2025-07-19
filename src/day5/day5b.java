7ac8b994285b11f6d72b9bb26fa18706fae3a474
hardik <hardikzode05@gmail.com>
2025-07-19 22:20:32 +0530

    Added 19th july practice

--- src/day5/day5b.java
+++ src/day5/day5b.java
@@ -1,6 +1,14 @@
 package day5;
 
 public class day5b {
+    public static void main(String[] args) {
+        //char At
+        String fullName= " Hardik zodetron";
 
+        //using char At
+        for(int i=0;i<fullName.length();i++){
+            System.out.println(fullName.charAt(i));
+        }
+    }
 
 }
