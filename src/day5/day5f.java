3cc8817eb24fa3d6065a60696495d1a59ff0bb49
hardik <hardikzode05@gmail.com>
2025-07-20 12:48:07 +0530

    Added 20th july practice

---
+++ src/day5/day5f.java
@@ -0,0 +1,21 @@
+//Reverse a string by the help of String Builder
+package day5;
+import java.util.*;
+
+public class day5f {
+    public static void main(String[] args) {
+         StringBuilder sb = new StringBuilder("hello");
+
+         for(int i=0; i<sb.length()/2;i++){
+             int front = i;
+             int back = sb.length()-1-i;  //5-1-0= >4
+
+             char frontChar= sb.charAt(i);
+             char backChar= sb.charAt(back);
+
+             sb.setCharAt(front,backChar);
+             sb.setCharAt(back,frontChar);
+         }
+         System.out.println(sb);
+    }
+}
