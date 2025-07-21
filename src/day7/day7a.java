8ee28e22cb629a750b01066545b563c655dc918a
hardik <hardikzode05@gmail.com>
2025-07-21 23:32:06 +0530

    Added 21st july practice

---
+++ src/day7/day7a.java
@@ -0,0 +1,9 @@
+//Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+package day7;
+import java.util.*;
+
+public class day7a {
+    public static void main(String[] args) {
+
+    }
+}
