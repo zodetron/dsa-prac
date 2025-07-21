e2b20d756d6a8fc11db00b6877c3b08ddf8303f8
hardik <hardikzode05@gmail.com>
2025-07-21 17:27:15 +0530

    Added 21st july practice

---
+++ src/day6/day6b.java
@@ -0,0 +1,31 @@
+//Selection Sort
+
+package day6;
+import java.util.*;
+public class day6b {
+
+    public static void printArray(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[]={8,7,4,1,2,0};
+        //Selection Sort
+        for(int i=0;i<arr.length-1;i++){
+            int smallest=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[smallest]>arr[j]){
+                    smallest=j;
+                }
+            }
+            int temp= arr[smallest];
+            arr[smallest]=arr[i];
+            arr[i]=temp;
+        }
+
+        printArray(arr);
+    }
+}
