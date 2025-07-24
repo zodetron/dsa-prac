a167ae47ce75e621170f2d02ff400612ad93a40b
hardik <hardikzode05@gmail.com>
2025-07-24 22:29:39 +0530

    Added 24th july practice

--- src/day8/day8a.java
+++ src/day8/day8a.java
@@ -1,5 +1,34 @@
 package day8;
 
-public class day8a {
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.*;
 
+public class day8a {
+    public static void main(String[] args) {
+//        char[]newArray=new char[4];
+//        List[]newArray=('xy','xyz','zxy','yz');
+
+        String str1= ("hi");
+        String str2=("hi");
+        if(str1==str2){
+            System.out.println("true");
+        }
+        else {
+            System.out.println("false");
+        }
+
+
+
+        List<String> words = new ArrayList<>(Arrays.asList("xy","xyz","zxy","yz"));
+
+
+
+
+
+
+
+
+    }
 }
