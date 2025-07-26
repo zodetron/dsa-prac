54fda2474709f6157dd367882b9a8c8239325c11
hardik <hardikzode05@gmail.com>
2025-07-27 00:02:12 +0530

    Added 26th july practice

--- src/day8/day8d.java
+++ src/day8/day8d.java
@@ -1,4 +1,23 @@
 package day8;
 
 public class day8d  {
+
+}
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num: nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        int maxCount=0;
+        int maxElement=nums[0];
+
+        for(Map.Entry<Integer,Integer>entry:map.entrySet()){
+            if(entry.getValue()>maxCount){
+                maxCount=entry.getValue();
+                maxElement=entry.getKey();
+            }
+        }
+        return new int[]{maxElement};
+    }
 }
