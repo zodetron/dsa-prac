73e6dc24b8e190d5e3a8ac2e6b351729d7f29209
hardik <hardikzode05@gmail.com>
2025-07-27 23:48:38 +0530

    Added 27th july practice

--- src/day8/day8g.java
+++ src/day8/day8g.java
@@ -1,4 +1,23 @@
 package day8;
 
+import java.util.Arrays;
+
 public class day8g {
+        public int[] productExceptSelf(int[] nums) {
+            int []answer = new int [nums.length];
+            for(int i=0;i<nums.length;i++){
+                for(int j=0;j<nums.length;j++){
+                    if(i!=j){
+                        answer[i]=i*j;
+                    }
+                }
+            }
+            return answer;
+        }
+    public static void main(String[] args) {
+        day8g sol = new day8g();
+        int[] nums = {1,2,3,4};
+        int[] result = sol.productExceptSelf(nums);
+        System.out.println(Arrays.toString(result));
+    }
 }
