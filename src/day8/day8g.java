08ba7601cbd108014a5357f10f3db64845146306
hardik <hardikzode05@gmail.com>
2025-07-28 22:03:34 +0530

    Added 28th july practice

--- src/day8/day8g.java
+++ src/day8/day8g.java
@@ -3,20 +3,25 @@ package day8;
 import java.util.Arrays;
 
 public class day8g {
-        public int[] productExceptSelf(int[] nums) {
-            int []answer = new int [nums.length];
-            for(int i=0;i<nums.length;i++){
-                for(int j=0;j<nums.length;j++){
-                    if(i!=j){
-                        answer[i]=i*j;
-                    }
+    public int[] productExceptSelf(int[] nums) {
+        int []answer = new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            int product = 1;
+            for(int j=0;j<nums.length;j++){
+                if(nums[j]==0){
+                    product=0;
+                }
+                else if(nums[i]!=nums[j]){
+                    product*=nums[j];
                 }
             }
-            return answer;
+            answer[i]=product;
         }
+        return answer;
+    }
     public static void main(String[] args) {
         day8g sol = new day8g();
-        int[] nums = {1,2,3,4};
+        int[] nums = {0,0};
         int[] result = sol.productExceptSelf(nums);
         System.out.println(Arrays.toString(result));
     }
