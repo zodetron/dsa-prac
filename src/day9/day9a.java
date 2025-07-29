4352e7d026f21c98b3df25988952d3c21b156ed0
hardik <hardikzode05@gmail.com>
2025-07-29 21:47:32 +0530

    Added 29th  july practice

--- src/day9/day9a.java
+++ src/day9/day9a.java
@@ -1,7 +1,23 @@
 package day9;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 public class day9a {
-    public static void main(String[] args) {
+    public String encode(List<String> strs) {
+        return ""; // temporary placeholder
+    }
 
+    public List<String> decode(String str) {
+        return new ArrayList<>(); // temporary placeholder
+    }
+
+    public static void main(String[] args) {
+        day9a sol= new day9a();
+        List<String> myList = new ArrayList<>();
+        List<String> strs = Arrays.asList("neet", "code", "love", "you");
+//        myList=sol.decode(strs);
+        myList = sol.decode(sol.encode(strs));
     }
 }
--- src/day9/day9b.java
+++ src/day9/day9b.java
@@ -1,7 +1,28 @@
+
 package day9;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-public class day9b {
-    public static void main(String[] args) {
 
-    }
-}
+        public class day9b {
+            public String encode(List<String> strs) {
+                String enco= "";
+                for(String s:strs){
+                    enco+=s;
+                }
+                return enco;
+            }
+
+            public List<String> decode(String str) {
+                return new ArrayList<>();
+            }
+
+            public static void main(String[] args) {
+                day9b sol= new day9b();
+                List<String> myList = new ArrayList<>();
+                List<String> strs = Arrays.asList("neet", "code", "love", "you");
+                myList = sol.decode(sol.encode(strs));
+                System.out.println(myList);
+            }
+        }
