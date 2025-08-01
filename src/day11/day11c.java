770316d60a8668ba010549b35c76b6d422ddcced
hardik <hardikzode05@gmail.com>
2025-08-01 22:35:37 +0530

    Added 1st August practice

--- src/day11/day11c.java
+++ src/day11/day11c.java
@@ -1,5 +1,22 @@
 package day11;
-
+import java.util.*;
+//implementation of stack using collection framework
+//easiest and shortest for interviews etcccc
 public class day11c {
 
+
+
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        s.push(5);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
 }
