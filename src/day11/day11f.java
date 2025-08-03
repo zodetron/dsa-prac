ddddc9d9e0755e52c0d59066a879969e6cbb83ad
hardik <hardikzode05@gmail.com>
2025-08-03 11:44:58 +0530

    Added 3rd August practice

---
+++ src/day11/day11f.java
@@ -0,0 +1,9 @@
+package day11;
+import java.util.HashMap;
+
+public class day11f {
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+    java.util.map<Character,Character>closeToOpen=new java.util.HashMap<>();
+}
