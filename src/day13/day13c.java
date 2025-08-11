604dfede6e20b47d7d8e793762b5d6016bbc3388
hardik <hardikzode05@gmail.com>
2025-08-11 23:23:18 +0530

    Added 11th August practice

--- src/day13/day13c.java
+++ src/day13/day13c.java
@@ -1,7 +1,23 @@
 package day13;
 
 public class day13c {
-    public static void main(String[] args) {
 
-    }
+            public int maxArea(int[] heights) {
+                int res = 0;
+                for (int i = 0; i < heights.length; i++) {
+                    for (int j = i + 1; j < heights.length; j++) {
+                        res = Math.max(res, Math.min(heights[i], heights[j]) * (j - i));
+                    }
+                }
+                return res;
+            }
+
+            public static void main(String[] args) {
+                day13c sol = new day13c();
+
+                // Example test case
+                int[] heights = {1,8,6,2,5,4,8,3,7};
+                System.out.println("Maximum area: " + sol.maxArea(heights)); // Expected: 49
+            }
+
 }
