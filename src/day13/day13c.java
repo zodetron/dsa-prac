b71df1d082a97e57bdc2e532fa2eb45d536586e3
hardik <hardikzode05@gmail.com>
2025-08-12 20:06:47 +0530

    Added 11th August practice

--- src/day13/day13c.java
+++ src/day13/day13c.java
@@ -4,6 +4,7 @@ public class day13c {
 
             public int maxArea(int[] heights) {
                 int res = 0;
+                int xyz=1;
                 for (int i = 0; i < heights.length; i++) {
                     for (int j = i + 1; j < heights.length; j++) {
                         res = Math.max(res, Math.min(heights[i], heights[j]) * (j - i));
