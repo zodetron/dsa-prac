c61cb8386dea9ccf214f65a6d28c6e88d70c0b8d
hardik <hardikzode05@gmail.com>
2025-08-28 23:38:14 +0530

    Added 28th August practice

--- src/day16/day16b.java
+++ src/day16/day16b.java
@@ -2,6 +2,27 @@ package day16;
 
 public class day16b {
     public static void main(String[] args) {
-
+//        class Solution {
+//            public String simplifyPath(String path) {
+//                Stack<String> stack = new Stack<>();
+//                for (String path : tokens) {
+//                    if (c.equals("//")) {
+//                        stack.pop();
+//                    }
+//                    else if(c.equals("..")){
+//                        stack.pop();
+//                        if(c.equals("/")){
+//                            continue;
+//                        }
+//                        else{
+//                            stack.pop();
+//                        }
+//                    }
+//                    else{
+//                        return stack.peek();
+//                    }
+//                }
+//            }
+//        }
     }
 }
