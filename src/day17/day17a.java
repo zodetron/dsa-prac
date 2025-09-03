00d6d573dd1f28a512807eb5998a3ed45b7c1519
hardik <hardikzode05@gmail.com>
2025-09-03 21:01:16 +0530

    Added 3rd September practice

--- src/day17/day17a.java
+++ src/day17/day17a.java
@@ -1,5 +1,6 @@
 package day17;
 //practise
+//day 17 is all about two pointers and its questions
 
 public class day17a {
     public static void main(String[] args) {
--- src/day17/day17e.java
+++ src/day17/day17e.java
@@ -1,4 +1,47 @@
 package day17;
+import java.util.*;
+
+//LeetCode 15 – 3Sum
 
 public class day17e {
+        public static List<List<Integer>> threeSum(int[] nums) {
+            Arrays.sort(nums);
+            List<List<Integer>> res = new ArrayList<>();
+
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] > 0) break;
+                if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+                int l = i + 1, r = nums.length - 1;
+                while (l < r) {
+                    int sum = nums[i] + nums[l] + nums[r];
+                    if (sum > 0) {
+                        r--;
+                    } else if (sum < 0) {
+                        l++;
+                    } else {
+                        res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                        l++;
+                        r--;
+                        while (l < r && nums[l] == nums[l - 1]) {
+                            l++;
+                        }
+                    }
+                }
+            }
+            return res;
+        }
+
+        public static void main(String[] args) {
+            int[] nums = {-1, 0, 1, 2, -1, -4};
+            List<List<Integer>> result = threeSum(nums);
+
+            System.out.println("Triplets that sum to zero:");
+            for (List<Integer> triplet : result) {
+                System.out.println(triplet);
+            }
+            // Expected Output:
+            // [-1, -1, 2]
+            // [-1, 0, 1]
+        }
 }
