415cf9de7c386ca6e4e0d8278eb314d6abe533d8
hardik <hardikzode05@gmail.com>
2025-09-03 20:52:06 +0530

    Added 3rd September practice

--- src/day17/day17b.java
+++ src/day17/day17b.java
@@ -1,7 +1,33 @@
 package day17;
 
 public class day17b {
-    public static void main(String[] args) {
 
+        public int removeElement(int[] nums, int val) {
+            int i = 0; // position for next valid element
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] != val) {
+                    nums[i] = nums[j];
+                    i++;
+                }
+            }
+            return i;
+        }
+    public static void main(String[] args) {
+        day17b sol = new day17b();
+
+        int[] nums = {3, 2, 2, 3};
+        int val = 3;
+
+        int newLength = sol.removeElement(nums, val);
+
+        System.out.println("New length: " + newLength);
+
+        System.out.print("Array after removal: ");
+        for (int i = 0; i < newLength; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        // Expected Output:
+        // New length: 2
+        // Array after removal: 2 2
     }
 }
