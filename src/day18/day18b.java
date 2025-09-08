b4561d15468445efba96cd404d827ce3c8c0b9d4
hardik <hardikzode05@gmail.com>
2025-09-08 21:53:11 +0530

    Added 8th September practice

--- src/day18/day18b.java
+++ src/day18/day18b.java
@@ -1,7 +1,32 @@
 package day18;
 
 public class day18b {
-    public static void main(String[] args) {
-    
-    }
+        public int searchInsert(int[] nums, int target) {
+            int left = 0, right = nums.length - 1;
+
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+
+                if (nums[mid] == target) {
+                    return mid; // found
+                } else if (nums[mid] < target) {
+                    left = mid + 1; // target in right half
+                } else {
+                    right = mid - 1; // target in left half
+                }
+            }
+
+            return left; // insert position
+        }
+
+        public static void main(String[] args) {
+            day18b sol = new day18b();
+            int[] nums = {1, 3, 5, 6};
+
+            System.out.println(sol.searchInsert(nums, 5)); // 2
+            System.out.println(sol.searchInsert(nums, 2)); // 1
+            System.out.println(sol.searchInsert(nums, 7)); // 4
+            System.out.println(sol.searchInsert(nums, 0)); // 0
+        }
+
 }
