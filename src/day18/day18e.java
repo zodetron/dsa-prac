5a97ab26df3de76614ffdfee335e61f13e38965f
hardik <hardikzode05@gmail.com>
2025-09-17 23:09:27 +0530

    Added 17th September practice

--- src/day18/day18e.java
+++ src/day18/day18e.java
@@ -1,7 +1,32 @@
 package day18;
+import java.util.*;
 
 public class day18e {
-    public static void main(String[] args) {
 
-    }
+        public static int lengthOfLongestSubstring(String s) {
+            Map<Character, Integer> map = new HashMap<>();
+            int left = 0, maxLen = 0;
+
+            for (int right = 0; right < s.length(); right++) {
+                char c = s.charAt(right);
+
+                if (map.containsKey(c)) {
+                    left = Math.max(left, map.get(c) + 1);
+                }
+
+                map.put(c, right);
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+            return maxLen;
+        }
+
+        public static void main(String[] args) {
+            String[] testCases = {"abcabcbb", "bbbbb", "pwwkew", "dvdf"};
+
+            for (String test : testCases) {
+                System.out.println("Input: " + test);
+                System.out.println("Longest Substring Length: " + lengthOfLongestSubstring(test));
+                System.out.println("------");
+            }
+        }
 }
