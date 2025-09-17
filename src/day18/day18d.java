896c8fd9bbd9cb8e8b6c09c8258739d037d38fbc
hardik <hardikzode05@gmail.com>
2025-09-17 23:08:21 +0530

    Added 17th September practice

--- src/day18/day18d.java
+++ src/day18/day18d.java
@@ -1,7 +1,29 @@
 package day18;
+import java.util.*;
 
 public class day18d {
-    public static void main(String[] args) {
-        int m =10;
+        public static int lengthOfLongestSubstring(String s) {
+            Set<Character> set = new HashSet<>();
+            int left = 0, maxLen = 0;
+
+            for (int right = 0; right < s.length(); right++) {
+                while (set.contains(s.charAt(right))) {
+                    set.remove(s.charAt(left));
+                    left++;
+                }
+                set.add(s.charAt(right));
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+            return maxLen;
+        }
+
+        public static void main(String[] args) {
+            String[] testCases = {"abcabcbb", "bbbbb", "pwwkew", "dvdf"};
+
+            for (String test : testCases) {
+                System.out.println("Input: " + test);
+                System.out.println("Longest Substring Length: " + lengthOfLongestSubstring(test));
+                System.out.println("------");
+            }
+        }
     }
-}
