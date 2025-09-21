0c90295bcd8df31d2a795c5deb9754c4bda6b507
hardik <hardikzode05@gmail.com>
2025-09-21 23:11:46 +0530

    Added 21st September practice

--- src/day20/day20e.java
+++ src/day20/day20e.java
@@ -1,5 +1,19 @@
 package day20;
+import java.util.Scanner;
 
 public class day20e {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("================================");
+            for (int i = 0; i < 3; i++) {
+                String s = sc.next();
+                int x = sc.nextInt();
+                System.out.printf("%-15s%03d%n", s, x);
+            }
+            System.out.println("================================");
+
+            sc.close();
+        }
 
 }
