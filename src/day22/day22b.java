72858c2732afa4abfe6a15e7c6e3d87d5cc1e5d2
hardik <hardikzode05@gmail.com>
2025-10-02 23:05:37 +0530

    Added 2nd Oct practice

--- src/day22/day22b.java
+++ src/day22/day22b.java
@@ -3,5 +3,9 @@ package day22;
 public class day22b {
     public static void main(String[] args) {
         int m=10;
+        int n=20;
+        for(int i=0;i<n;i++){
+            System.out.println(i);
+        }
     }
 }
