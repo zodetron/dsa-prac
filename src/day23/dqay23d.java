c90f6571466e20f994e5a1d669aee20f3ad736e6
hardik <hardikzode05@gmail.com>
2025-10-04 22:47:49 +0530

    Added 4th Oct practice

---
+++ src/day23/dqay23d.java
@@ -0,0 +1,30 @@
+package day23;
+//leetcode 49 - group anagrams
+import java.util.*;
+
+class day23d {
+
+        public List<List<String>> groupAnagrams(String[] strs) {
+            Map<String, List<String>> res = new HashMap<>();
+
+            for (String s : strs) {
+                char[] charArray = s.toCharArray();
+                Arrays.sort(charArray);
+                String sortedS = new String(charArray);
+
+                res.putIfAbsent(sortedS, new ArrayList<>());
+                res.get(sortedS).add(s);
+            }
+
+            return new ArrayList<>(res.values());
+        }
+
+        public static void main(String[] args) {
+            day23d sol = new day23d();
+
+            String[] input = {"eat", "tea", "tan", "ate", "nat", "bat"};
+            List<List<String>> result = sol.groupAnagrams(input);
+
+            System.out.println("Grouped Anagrams: " + result);
+        }
+}
