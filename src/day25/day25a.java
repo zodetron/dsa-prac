0073c3f8bb893606d9748d6059c1aa6f6a25eea9
hardik <hardikzode05@gmail.com>
2025-10-25 23:14:23 +0530

    Added 25th Oct practice

--- src/day25/day25a.java
+++ src/day25/day25a.java
@@ -3,5 +3,9 @@ package day25;
 public class day25a {
     public static void main(String[] args) {
             //int m-10;
+        int a=10
+        for(int i=1;i<10;i++){
+            a=a+1;
+        }
     }
 }
