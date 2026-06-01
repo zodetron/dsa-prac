987eea41008f028c1b863e6495ab348550a05ab6
zodetron <hardikzode05@gmail.com>
2026-06-01 10:43:55 +0530

    bubbly sort

---
+++ src/CRT/Day5b.java
@@ -0,0 +1,19 @@
+package CRT;
+//merge sort
+//divide and conquer
+public class Day5b {
+
+    static void mergeSort(int arr[],int si,int ei){
+        if (si>=ei){
+            return;
+        }
+        int mid = si + (ei-si)/2;
+        divide(arr,si,mid)
+        divide(arr,mid+1, ei)
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5,6,4354,32,536,54,76453,3,57,8,76,74235,4,678,68,57,65,432,7,856,74,352,34,75};
+        int n = arr.length;
+    }
+}
