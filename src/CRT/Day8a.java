3d755fb23ff2476b101722cf85bba44a4962b29b
zodetron <hardikzode05@gmail.com>
2026-06-06 10:59:34 +0530

    local changes

---
+++ src/CRT/Day8a.java
@@ -0,0 +1,24 @@
+package CRT;
+//revering array with two pointer
+
+public class Day8a {
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4,5,6,7,8,9,0};
+        int left=0;
+        int right=arr.length;
+//        int mid = arr.length/2;
+        int temp=0;
+        for (int i =0;i<arr.length/2;i++){
+            int arr[right]=0;
+            int arr[right]=temp;
+            int arr[right]=arr[left]
+            int arr[left]=arr[right]
+                    left++
+                            right--;
+
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]);
+        }
+    }
+}
