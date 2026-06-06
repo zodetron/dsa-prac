ab7a475cf28ef5b6d69e859c685e28d1333f1b79
zodetron <hardikzode05@gmail.com>
2026-06-06 11:00:58 +0530

    reverse array with two pointers

--- src/CRT/Day8a.java
+++ src/CRT/Day8a.java
@@ -1,24 +1,26 @@
 package CRT;
-//revering array with two pointer
+// reversing array with two pointers
 
 public class Day8a {
     public static void main(String[] args) {
-        int [] arr = {1,2,3,4,5,6,7,8,9,0};
-        int left=0;
-        int right=arr.length;
-//        int mid = arr.length/2;
-        int temp=0;
-        for (int i =0;i<arr.length/2;i++){
-            int arr[right]=0;
-            int arr[right]=temp;
-            int arr[right]=arr[left]
-            int arr[left]=arr[right]
-                    left++
-                            right--;
+        int[] arr = {1,2,3,4,5,6,7,8,9,0};
 
+        int left = 0;
+        int right = arr.length - 1;
+        int temp;
+
+        while (left < right) {
+
+            temp = arr[right];
+            arr[right] = arr[left];
+            arr[left] = temp;
+
+            left++;
+            right--;
         }
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
     }
 }
