d9f026f6adcb8c662651fe3c5f2a90e206964dc1
zodetron <hardikzode05@gmail.com>
2026-06-07 23:35:50 +0530

    string builder pracs

---
+++ src/Phase2_Day2/Day2b.java
@@ -0,0 +1,9 @@
+package Phase2_Day2;
+
+public class Day2b {
+    public static void main(String[] args) {
+        String str = new String("Hello");
+        StringBuilder sb = new StringBuilder(str);
+        System.out.println(sb.capacity());
+    }
+}
