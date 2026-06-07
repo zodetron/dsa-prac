f5be27dd0d91c6babbe3a34eeb666df32a02c2e2
zodetron <hardikzode05@gmail.com>
2026-06-07 23:39:52 +0530

    string builder pracs

---
+++ src/Phase2_Day2/Day2d.java
@@ -0,0 +1,24 @@
+package Phase2_Day2;
+
+public class Day2d {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        Day2d solution = new Day2d();
+        char[] s = {'a', 'b'};
+        solution.reverseString(s);
+        System.out.println(s);
+    }
+}
