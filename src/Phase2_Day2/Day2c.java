dceb72f17896c5b7c9efc114c6bd010cd912ba46
zodetron <hardikzode05@gmail.com>
2026-06-07 23:40:30 +0530

    string builder pracs

--- src/Phase2_Day2/Day2c.java
+++ src/Phase2_Day2/Day2c.java
@@ -1,16 +1,17 @@
 package Phase2_Day2;
 //leetcode  344 reverse string = not best solution
 public class Day2c {
+    public static void reverseString(char[] s) {
+        StringBuilder sb = new StringBuilder(new String(s));
+        sb.reverse();
+        char[] reversed = sb.toString().toCharArray();
+        for (int i = 0; i < s.length; i++) {
+            s[i] = reversed[i];
+        }
+    }
     public static void main(String[] args) {
-        String s = new String ("jelloooo");
-                StringBuilder sb = new StringBuilder(new String(s));
-                sb.reverse();
-
-                char[] s1 = sb.toString().toCharArray();
-
-                for(int i = 0; i < s.length; i++) {
-                    s[i] = s1[i];
-                }
-            }
+        char[] s = {'j','e','l','l','o','o','o','o'};
+        reverseString(s);
+        System.out.println(s);
     }
 }
