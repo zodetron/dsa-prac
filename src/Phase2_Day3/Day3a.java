40cf371a798cc4b1a3eec059b271e578903470de
zodetron <hardikzode05@gmail.com>
2026-06-17 23:42:32 +0530

    string builder pracs

--- src/Phase2_Day3/Day3a.java
+++ src/Phase2_Day3/Day3a.java
@@ -1,4 +1,27 @@
 package Phase2_Day3;
 
 public class Day3a {
+        public String removeDuplicates(String s) {
+            StringBuilder sb = new StringBuilder();
+
+            for (char ch : s.toCharArray()) {
+                if (sb.length() > 0 && sb.charAt(sb.length() - 1) == ch) {
+                    sb.deleteCharAt(sb.length() - 1);
+                } else {
+                    sb.append(ch);
+                }
+            }
+
+            return sb.toString();
+        }
+
+        public static void main(String[] args) {
+            Day3a sol = new Day3a();
+
+            String s = "abbaca";
+            String result = sol.removeDuplicates(s);
+
+            System.out.println("Input: " + s);
+            System.out.println("Output: " + result);
+        }
 }
